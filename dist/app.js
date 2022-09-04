23e4d86d95965d86e2f5c27d57424e7d9a490cec
Ernestoyoofi <yoofi.ernesto@gmail.com>
2022-09-04 21:48:30 +0700

    Tambahin form buat isi url

--- dist/app.js
+++ dist/app.js
@@ -2,7 +2,7 @@ const urls = new URLSearchParams(window.location.search).get("url")
 
 if(!urls) {
   console.log("URL ?")
-  document.querySelector(".loading_prepar").innerHTML = "<span>! Url Tidak Ada</span>"
+  document.querySelector(".loading_prepar").innerHTML = `<span>URL ?</span> <form action="index.html"><input type="url" name="url" placeholder="https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/examples/learning/helloworld.pdf"><input type="hidden" name="inputtype" value="focusing_by_form"></form>`
 } else {
   let iv = -1
   for(let i in urls.split("/")) { iv++ }
